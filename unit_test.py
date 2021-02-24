3cc57ecc125f529fee02c832d61683181dd74308
Kunhao Z <kunhao.zheng@inria.fr>
2021-02-24 23:33:46 +0100

    transformer 0.9

--- unit_test.py
+++ unit_test.py
@@ -1,4 +1,4 @@
-from helper import read_x_train, fill_nan, dataset_preparation_for_rnn, limit_max_length, rebalance_data
+from helper import read_x_train, fill_nan, dataset_preparation_for_rnn, limit_max_length, rebalance_data, padding_with_0
 import pandas as pd
 from collections import Counter
 
@@ -37,4 +37,22 @@ def test_rebalance():
     print(Counter([tuple(data) for data in label_train_data]))
 
 
-test_rebalance()
+def test_padding():
+    x_train = read_x_train('data/AMF_train_X.csv',
+                           includeShare=True, includeDay=True)
+    x_train = fill_nan(x_train)
+    y_train = pd.read_csv('data/AMF_train_Y.csv')
+
+    trader_train_data, label_train_data = dataset_preparation_for_rnn(
+        x_train, y_train)
+
+    trader_train_data, label_train_data = limit_max_length(
+        trader_train_data, label_train_data)
+    trader_train_data, label_train_data = rebalance_data(
+        trader_train_data, label_train_data)
+    trader_train_data = padding_with_0(trader_train_data)
+    length_set = set([data.shape for data in trader_train_data])
+    print(length_set)
+
+
+test_padding()
